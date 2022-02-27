f40a4cf2fc648a44c94dacc11470899ca3640a4e
lisz1012 <lisz1012@163.com>
2022-02-27 00:20:37 -0800

    Add notes

--- src/main/scala/com/lisz/stream/transformation2/RebalanceTest.scala
+++ src/main/scala/com/lisz/stream/transformation2/RebalanceTest.scala
@@ -5,6 +5,7 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 增大分区数
 shuffle - 随机
 rebalance - 轮询
+两者在数据量比较大的时候都能够平均分配
  */
 object RebalanceTest {
   def main(args: Array[String]): Unit = {
