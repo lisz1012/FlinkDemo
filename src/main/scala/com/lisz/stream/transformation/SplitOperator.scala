4809b08f3bcd547f20a9c1f800c7648372d34bb9
lisz1012 <lisz1012@163.com>
2023-08-14 22:50:25 -0700

    Reformat

--- src/main/scala/com/lisz/stream/transformation/SplitOperator.scala
+++ src/main/scala/com/lisz/stream/transformation/SplitOperator.scala
@@ -2,7 +2,7 @@ package com.lisz.stream.transformation
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
-// split算子可以根据某些条件拆分数据流
+// split算子可以根据某些条件拆分数据流.
 object SplitOperator {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
