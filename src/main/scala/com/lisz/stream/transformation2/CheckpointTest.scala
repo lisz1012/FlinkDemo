47db91d2575d0ee60edb99331c4f5193ffd8aded
lisz1012 <lisz1012@163.com>
2023-08-18 23:30:42 -0700

    Reformat

--- src/main/scala/com/lisz/stream/transformation2/CheckpointTest.scala
+++ src/main/scala/com/lisz/stream/transformation2/CheckpointTest.scala
@@ -7,7 +7,7 @@ object CheckpointTest {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     val stream = env.fromCollection(List(1, 2, 3, 4, 5, 6, 7, 8, 9))
-    stream.keyBy(x => x % 2).reduce(_+_).print // 肯定只有俩分区
+    stream.keyBy(x => x % 2).reduce(_+_).print // 肯定只有俩分区.
     env.execute
   }
 }
