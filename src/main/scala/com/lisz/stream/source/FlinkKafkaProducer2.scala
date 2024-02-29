584863801fde211d3c943212710ddb210f53de3c
lisz1012 <lisz1012@163.com>
2024-02-28 23:57:16 -0800

    Short URL 生产者 OK 了

--- src/main/scala/com/lisz/stream/source/FlinkKafkaProducer2.scala
+++ src/main/scala/com/lisz/stream/source/FlinkKafkaProducer2.scala
@@ -15,7 +15,7 @@ object FlinkKafkaProducer2 {
     properties.setProperty("value.serializer", classOf[StringSerializer].getName)
 
     val producer = new KafkaProducer[String, String](properties)
-    // send是个异步，所以要么睡一会儿，要么Future.get一下，以防止还没send出去main就结束了，结果就跟没运行一样.
+    // send是个异步，所以要么睡一会儿，要么Future.get一下，以防止还没send出去main就结束了，结果就跟没运行一样
     val future = producer.send(new ProducerRecord[String, String]("long-url", "aaabbb localhost"))
     val recordMetadata = future.get
 
